7dab407d94615189dfc2c9f6a87a99546c967cb3
pfc2026 <pfc2026ifpr@gmail.com>
2026-06-01 10:59:05 -0300

    Delete server.cjs

--- server.cjs
+++
@@ -1,3 +0,0 @@
-// server.cjs — ponto de entrada alternativo (CommonJS explícito).
-// Usado quando package.json define "type": "module" mas server ainda precisa de CJS.
-module.exports = require('./api/_app');
